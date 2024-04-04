6d6a31115afcd222f3f5cc537a69175d5c0159db
arturpgalvao <164554675+arturpgalvao@users.noreply.github.com>
2024-04-03 22:17:19 -0300

    Create questao18.c

---
+++ questoes/questao18.c
@@ -0,0 +1,31 @@
+/* 
+TEMPO DE EXECUÇÃO DA QUESTÃO 16:
+
+Days              : 0
+Hours             : 0
+Minutes           : 0
+Seconds           : 0
+Milliseconds      : 19
+Ticks             : 195220
+TotalDays         : 2,25949074074074E-07
+TotalHours        : 5,42277777777778E-06
+TotalMinutes      : 0,000325366666666667
+TotalSeconds      : 0,019522
+TotalMilliseconds : 19,522
+
+TEMPO DE EXECUÇÃO DA QUESTÃO 17:
+
+Days              : 0
+Hours             : 0
+Minutes           : 0
+Seconds           : 0
+Milliseconds      : 20
+Ticks             : 203062
+TotalDays         : 2,35025462962963E-07
+TotalHours        : 5,64061111111111E-06
+TotalMinutes      : 0,000338436666666667
+TotalSeconds      : 0,0203062
+TotalMilliseconds : 20,3062
+
+Utilizando a função measure-command do PowerShell do Windows, esses foram os resultados obtidos. Em média, as execuções demoraram mais ou menos o mesmo tempo
+*/
